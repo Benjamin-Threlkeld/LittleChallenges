a9200fdd922944feecdd838be5c524e25c68baa0
Benjamin-Threlkeld <benjamin.threlkeld@gmail.com>
2014-02-21 22:20:56 -0600

    Added a Fibonacci generator that a friend and I were puzzling our selves with.

---
+++ Javascript/Fibonacci.js
@@ -0,0 +1,27 @@
+function fibonacciSequence(limit) {
+	var currentFibonacci = 0,
+		lastFibonacci = 0,
+		fibonacciNumbers = [];
+		limit = limit || 10;
+
+	for (i = 0; i < limit; i += 1) {
+
+		newCurrentFibonacci = currentFibonacci + lastFibonacci;
+		
+		currentFibonacci = lastFibonacci;
+		
+		lastFibonacci = newCurrentFibonacci;
+		
+		fibonacciNumbers.push(newCurrentFibonacci);
+
+		if (lastFibonacci === 0 && currentFibonacci === 0) {
+			currentFibonacci = 1;
+		}
+
+	}
+	
+	return fibonacciNumbers;
+
+}
+
+console.log( fibonacciSequence() );
